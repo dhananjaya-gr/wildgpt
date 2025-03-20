a578e526405a019e4e3accfba4f16c3012a278ba
SRI SAI PAMU <srpamu@cisco.com>
2025-03-20 17:50:39 +0530

    updated frontend and requirements.txt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       ></div>
       <div className="flex flex-col min-h-screen w-full max-w-3xl mx-auto px-4 bg-white bg-opacity-70 backdrop-blur-md relative z-10 rounded-lg">
         <header className="sticky top-0 shrink-0 z-20">
-          <div className="flex items-center justify-between h-full w-full pt-4 pb-2">
+          <div className="flex items-center justify-between h-full w-full pt-4 pb-2 bg-white bg-opacity-70 backdrop-blur-md relative z-10 rounded-lg">
             <img src={logo} className="w-32" alt="logo" />
             <h1 className="font-urbanist text-[1.65rem] font-semibold text-center flex-grow">
               Wildlife AI Chatbot
