61de6484db9d7996ce6954c1b5e10037bfef6266
dhananjaya-gr <grdhananjaya540@gmail.com>
2025-03-20 15:28:32 +0530

    Extract links from pdfs

--- hackoholic_bot.py
+++ hackoholic_bot.py
@@ -7,6 +7,8 @@
 # MODEL = 'Hackaholics'
 
 import os
+import re
+import fitz
 import ollama
 import logging
 from langchain_community.document_loaders import UnstructuredPDFLoader
@@ -19,21 +21,55 @@ from langchain_ollama import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_ollama import OllamaEmbeddings
+
+# Constants
 EMBEDDING_MODEL = 'nomic-embed-text'
-VECTOR_STORE_NAME='simple_rag'
+VECTOR_STORE_NAME = 'simple_rag'
 MODEL = 'llama3.1'
 
-#logging.basicConfig(level=logging.INFO)
+# Set logging level
 logging.basicConfig(level=logging.WARNING)
 
+# Folder containing Index of all PDF documents
 DOC_FOLDER = os.path.join(os.path.dirname(__file__), "Index/")
+
+def extract_links_from_pdf(pdf_path):
+    """
+    Extracts all hyperlinks from a given PDF file from all the pages.
+
+    :param pdf_path: The file path to the PDF document.
+    :return: A list of unique URLs found in the PDF.
+    """
+    links = []
+    # Open the PDF
+    pdf_document = fitz.open(pdf_path)
+
+    # Iterate through all pages
+    for page_num in range(len(pdf_document)):
+        page = pdf_document[page_num]
+
+        # Extract text to search for embedded links
+        text = page.get_text("text")
+
+        # Find all URLs in the text using regex
+        urls = re.findall(r'https?://[^\s]+', text)
+        links.extend(urls)
+
+        # Extract hyperlinks from annotations
+        for link in page.get_links():
+            if 'uri' in link:
+                links.append(link['uri'])
+    pdf_document.close()
+    # Remove duplicates
+    return list(set(links))
 
 def ingest_pdf(doc_path, folder=False):
     """
-    Ingests a PDF file and returns the text content.
+    Ingests a PDF file or a folder of PDF files and returns the text content.
 
-    :param doc_path: the path to the PDF file
-    :return: the text content of the PDF file
+    :param doc_path: the path to the PDF file or folder
+    :param folder: boolean indicating if the path is a folder
+    :return: the text content of the PDF file(s)
     """
     if not folder:
         if doc_path.startswith('http'):
@@ -51,13 +87,17 @@ def ingest_pdf(doc_path, folder=False):
             return None
     else:
         data_pool = list()
+        all_links = []
         for file in os.listdir(doc_path):
             if file.endswith('.pdf'):
-                loader = UnstructuredPDFLoader(doc_path + file)
+                loader = UnstructuredPDFLoader(os.path.join(doc_path, file))
+                links = extract_links_from_pdf(os.path.join(doc_path, file))
                 data = loader.load()
                 logging.info(f"Unstructured PDF Loaded successfully {file}")
                 data_pool.append(data[0])
-        return data_pool
+                all_links.extend(links)
+        unique_links = list(set(all_links))
+        return data_pool, unique_links
 
 def split_documents(documents):
     """
@@ -129,9 +169,12 @@ def main():
     # data = ingest_pdf(DOC_PATH)
 
     # Load the PDF files from folder
-    data = ingest_pdf(DOC_FOLDER, folder=True)
+    data, links = ingest_pdf(DOC_FOLDER, folder=True)
     if data is None:
         return
+
+    if links:
+        pass
 
     # Split the documents
     chunks = split_documents(data)
